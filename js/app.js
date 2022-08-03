7f4ed31b5c55ca8045d80cb163c7507899afbeb9
loonhtet <gurdamanson@gmail.com>
2022-08-03 13:59:38 +0630

    3/8/22

---
+++ js/app.js
@@ -0,0 +1,26 @@
+$(document).ready(() => {
+  //    Start Header Section
+  //    Start Banner Section
+  $(".carousel").carousel({
+    interval: 3000,
+  });
+  //    End Banner Section
+  //    End Header Section
+
+  //   Start Info Section
+
+  $(window).scroll(function () {
+    let getscrollpoint = $(this).scrollTop();
+    console.log(getscrollpoint);
+
+    if (getscrollpoint >= 300) {
+      $(".infotexts").addClass("fromlefts");
+      $(".infopics").addClass("fromrights");
+    } else {
+      $(".infotexts").removeClass("fromlefts");
+      $(".infopics").removeClass("fromrights");
+    }
+  });
+
+  //   End Info Section
+});
